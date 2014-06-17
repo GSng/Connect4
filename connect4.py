82b5fe28cec20d27a05d49bf56c6a4a0d7847a3a
GSng <g_singhal@Gaurav-Singhals-MacBook-Air.local>
2014-06-16 22:59:24 -0400

    simple connect 4 game

---
+++ connect4.py
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: g_singhal
+"""
+
+import random
+
+def printboard(board):
+    print "   1   2   3   4    5   6   7"
+    print "1: "+board[0][0]+" | "+board[0][1]+" | "+board[0][2]+" | "+board[0][3]+" | "+board[0][4]+" | "+board[0][5]+" | "+board[0][6]
+    print "  ---+---+---+---+---+---+---"
+    print "2: "+board[1][0]+" | "+board[1][1]+" | "+board[1][2]+" | "+board[1][3]+" | "+board[1][4]+" | "+board[1][5]+" | "+board [1][6]  
+    print "  ---+---+---+---+---+---+---+"
+    print "3: "+board[2][0]+" | "+board[2][1]+" | "+board[2][2]+" | "+board[2][3]+" | "+board [2][4]+" | "+board [2][5]+" | "+board [2][6]
+    print "  ---+---+---+---+---+---+---+"
+    print "4: "+board[3][0]+" | "+board[3][1]+" | "+board[3][2]+" | "+board[3][3]+" | "+board [3][4]+" | "+board [3][5]+" | "+board [3][6]
+    print "  ---+---+---+---+---+---+---+"
+    print "5: "+board[4][0]+" | "+board[4][1]+" | "+board[4][2]+" | "+board[4][3]+" | "+board [4][4]+" | "+board [4][5]+" | "+board [4][6]
+    print "  ---+---+---+---+---+---+---+"
+    print "6: "+board[5][0]+" | "+board[5][1]+" | "+board[5][2]+" | "+board[5][3]+" | "+board [5][4]+" | "+board [5][5]+" | "+board [5][6]
+    print "  ---+---+---+---+---+---+---+"
+    print "7: "+board[6][0]+" | "+board[6][1]+" | "+board[6][2]+" | "+board[6][3]+" | "+board [6][4]+" | "+board [6][5]+" | "+board [6][6]
+    
+def check4win(board):
+    #check rows
+    for row in range(7):
+        for col in range(4):
+            if board[row][col]!="":
+                if board[row][col]==board[row][col+1]==board[row][col+2]==board[row][col+3]:
+                    return board[row][col]
+    
+    #check columns
+    for col in range(7):
+        for row in range(4):
+            if board[row][col]!="":
+                if board[row][col]==board[row+1][col]==board[row+2][col]==board[row+3][col]:
+                    return board[row][col]
+    
+    #check diagonals from top left
+    for row in range(4):
+        for col in range(4):
+            if board[row][col]!="":
+                if board[row][col]==board[row+1][col+1]==board[row+2][col+2]==board[row+3][col+3]:
+                    return board[row][col]
+    
+    #check diagonals from top right
+    for row in range(4):
+        for col in range(3,7):
+            if board[row][col]!="":
+                if board[row][col]==board[row+1][col-1]==board[row+2][col-2]==board[row+3][col-3]:
+                    return board[row][col]
+    
+    # if no winner
+    return ""
+
+def playhuman(board):
+    inputflag = True    
+    while inputflag:
+        row = input("Desired row (1-7) for next move: ")
+        col = input("Desired column (1-7) for next move: ")
+        
+        if row.isdigit() and col.isdigit:
+            if 1<=row<=7 and 1<=col<=7:
+                if board[row-1][col-1]=="":
+                    board[row-1][col-1] = "X"                
+                    inputflag = False                    
+                    break
+                else:
+                    print "row/column occupied. Try Again!\n"
+            else:
+                print "Invalid row/column. Try Again!\n"
+        else: 
+            print "Integer input only. Try Again!\n"
+            
+def playcomputer(board):
+    #get valid moves
+    validmoves = [(row,col) for row in range(6) for col in range(6) if board[row,col]==""]
+    
+    #check/score moves
+    movescores = []
+    for move in validmoves:
+        row = move[0]
+        col = move[1]
+        
+        board[row][col] = "O"
+        if check4win(board)=="O":
+            bestmove = move
+            break
+        else:
+            board[row][col] = ""
+            
+            #score moves ? <--add stuff here
+            
+            movescores.append(score)
+    
+    #choose move with max score
+    if movescores<>[]:
+        indx = movescores.index(max(movescores))
+        bestmove = validmoves[indx]
+        
+    print "The computer has chosen row=",str(bestmove[0])," and col=",str(bestmove[1])," \n"
+
+def main():
+    #create board
+    board = [["","","","","","",""],["","","","","","",""],["","","","","","",""],["","","","","","",""],["","","","","","",""],["","","","","","",""],["","","","","","",""]]
+    maxmoves = 7*7    
+    nummoves = 0    
+    
+    #choose who plays first
+    inputFlag = True
+    while inputFlag:
+        choice = raw_input("Do you want to play first? (y or n): ")
+        choice = choice[0]
+        if choice=="y" or choice=="Y":
+            playfirst = True
+            inputFlag = False
+            break
+        elif choice=="n" or choice=="N":
+            playfirst = False
+            inputFlag = False
+            break
+        else:
+            print "Invalid Input. Try Again!\n"
+    
+    #start gameplay
+    nowinordraw = True
+    while nowinordraw:
+        nummoves+=1
+        if nummoves<=maxmoves:
+            printboard(board)            
+            if nummoves%2==0:
+                if playfirst:
+                    playcomputer(board)
+                else:
+                    playhuman(board)
+            else:
+                if playfirst:
+                    playhuman(board)
+                else:
+                    playcomputer(board)
+        else:
+            print "All squares filled. Game is a draw! \n"
+            nowinordraw = False
+            break
+        
+        #check for winner
+        winner = check4win(board)
+        if winner=="X":
+            print "Congrats You have won! \n"
+            nowinordraw = False
+            break
+        elif winner=="O": 
+            print "Sorry, the computer has won :( \n"
+            nowinordraw = False
+            break
+            
+                    
+
